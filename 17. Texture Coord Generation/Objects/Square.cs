a226535fc0f9ea22764fea907bbcf3973d22b47f
Swiftless <swiftless@gmail.com>
2023-04-07 10:30:35 +0930

    17. Texture Coord Generation

---
+++ 17. Texture Coord Generation/Objects/Square.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+
+namespace _17.Objects
+{
+    internal static class Square
+    {
+        public static Dictionary<string, List<Vector3>> Vertices => new() {
+            {
+                "Front", new() {
+                    new Vector3(-1, -1, -1),
+                    new Vector3(1, -1, -1),
+                    new Vector3(1, 1, -1),
+                    new Vector3(-1, 1, -1)
+                }
+            }
+        };
+
+        public static Dictionary<string, List<Vector2>> UVs => new() {
+            {
+                /////////////////////////////////////////
+                ///
+                ///   This is how texture coordinates are arranged
+                ///
+                ///   0,1  x —–- x  1,1
+                ///        |     |
+                ///        |     |
+                ///        |     |
+                ///   0,0  x —–- x  1,0
+                ///
+                /////////////////////////////////////////
+                "Front", new() {
+                    new Vector2(0, 1),
+                    new Vector2(1, 1),
+                    new Vector2(1, 0),
+                    new Vector2(0, 0)
+                }
+            }
+        };
+    }
+}
